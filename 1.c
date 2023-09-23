a7183b206efe92d8a7e5884f697bb813116def2a
Lubesh <lubeshkumarsharma@gmail.com>
2023-09-23 13:28:19 +0530

    Max_sum_subarray full code

--- 1.c
+++ 1.c
@@ -2,11 +2,74 @@
 #include <math.h>
 #include <stdlib.h>
 #include <string.h>
-int cal_time(char s1[], char s2[])
+
+struct node
 {
-    int time = ((int)(s2[8] - s1[8]) * 10 + (int)(s2[9] - s1[9])) * 24 + (int)(s2[11] - s1[11]) * 10 + (int)(s2[12] - s1[12]);
-    return time;
+    int left;
+    int right;
+    int sum;
+};
+
+struct node maxcross(int l, int m, int r, int arr[])
+{
+    struct node ans;
+    int left_sum, right_sum, sum;
+    sum = 0;
+    left_sum = right_sum = -1 * __INT16_MAX__;
+    int i;
+    for (i = m; i >= l; i--)
+    {
+        sum = sum + arr[i];
+        if (sum > left_sum)
+        {
+            left_sum = sum;
+            ans.left = i;
+        }
+    }
+    sum = 0;
+    for (i = m + 1; i <= r; i++)
+    {
+        sum = sum + arr[i];
+        if (sum > right_sum)
+        {
+            right_sum = sum;
+            ans.right = i;
+        }
+    }
+    ans.sum = left_sum + right_sum;
+    return ans;
 }
+
+struct node solve(int leftt, int rightt, int arr[])
+{
+    struct node ans;
+    if (leftt == rightt)
+    {
+        ans.left = leftt;
+        ans.right = rightt;
+        ans.sum = arr[leftt];
+        return ans;
+    }
+    struct node lefta, righta, crossa;
+    int middle = (leftt + rightt) / 2;
+    lefta = solve(leftt, middle, arr);
+    righta = solve(middle + 1, rightt, arr);
+    if (lefta.sum > righta.sum)
+    {
+        ans = lefta;
+    }
+    else
+    {
+        ans = righta;
+    }
+    crossa = maxcross(leftt, middle, rightt, arr);
+    if (crossa.sum > ans.sum)
+    {
+        ans = crossa;
+    }
+    return ans;
+}
+
 int main()
 {
 #ifndef ONLINE_JUDGE
@@ -14,47 +77,20 @@ int main()
     freopen("output.txt", "w", stdout);
 #endif
 
-    char s1[17], s2[17];
-    scanf("%s %s", s1, s2);
-    printf("%s\n%s\n", s1, s2);
-    int time = cal_time(s1, s2);
-    printf("time = %d\n", time);
-    int hour = (int)(s1[11] - '0') + (int)(s1[12] - '0');
-    printf("hour = %d\n", hour);
-    int w, j;
-    scanf("%d %d", &w, &j);
-    int count = 0;
-    while (1)
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    int i;
+    for (i = 0; i < n; i++)
     {
-        if (time == 0)
-        {
-            break;
-        }
-        if (time > 0 && ((hour + w) % 24) != 0)
-        {
-            if (count == 1)
-            {
-                printf("no\n");
-                break;
-            }
-            time = time - w;
-            // printf("time = %d\n", time);
-            hour = (hour + w) % 24;
-            // printf("hour = %d\n", hour);
-            printf("w\n");
-        }
-        else
-        {
-            if (time < 0)
-            {
-                count = 1;
-            }
-            time = time + j;
-            // printf("time = %d\n", time);
-            hour = (hour - j + 24) % 24;
-            // printf("hour = %d\n", hour);
-            printf("a\n");
-        }
+        scanf("%d", &arr[i]);
+    }
+    struct node ans;
+    ans = solve(0, n - 1, arr);
+    printf("left=%d\nright=%d\nsum=%d\n", ans.left, ans.right, ans.sum);
+    for (i = ans.left; i <= ans.right; i++)
+    {
+        printf("%d ", arr[i]);
     }
     return 0;
 }
